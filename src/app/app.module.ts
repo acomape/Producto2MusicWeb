9baa5925a7a0ca6a8318e7d486ca881852639efc
alexgallardo11 <alexgallardo11@gmail.com>
2022-10-14 16:26:02 +0200

    cambios alex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CancionComponent } from './cancion/cancion.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent, CancionComponent
+    AppComponent, CancionComponent, NavbarComponent, HomeComponent
   ],
   imports: [
     BrowserModule,
