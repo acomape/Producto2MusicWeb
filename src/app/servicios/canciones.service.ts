03d23dccdaffc5b83e6d272b42d15e558253f7fa
acomape <100797506+acomape@users.noreply.github.com>
2022-11-17 22:27:49 +0100

    añadidoDuracion

--- src/app/servicios/canciones.service.ts
+++ src/app/servicios/canciones.service.ts
@@ -17,7 +17,8 @@ export class CancionesService {
       fecha: "2013",
       album: "R.A.M",
       url: "assets/instantcrush.mp3",
-      description:"Un clásico de la música electrónica.¿Quién no conoce a Daft Punk?"
+      description:"Un clásico de la música electrónica.¿Quién no conoce a Daft Punk?",
+      duration:"05:29",
     },
     {
       id: 2,
@@ -28,7 +29,8 @@ export class CancionesService {
       fecha: "2022",
       album: "Motomami",
       url: "assets/despecha.mp3",
-      description:"Lo más nuevo de la cantante que lo está petando todo."
+      description:"Lo más nuevo de la cantante que lo está petando todo.",
+      duration:"02:38",
     },
     {
       id: 3,
@@ -39,7 +41,8 @@ export class CancionesService {
       fecha: "2022",
       album: "Un Verano sin ti",
       url: "assets/unveranosinti.mp3",
-      description:"El latineo de Bad Bunny por excelencia"
+      description:"El latineo de Bad Bunny por excelencia",
+      duration:"02:28",
     },
     {
       id: 4,
@@ -50,7 +53,8 @@ export class CancionesService {
       fecha: "2019",
       album: "Ascend",
       url: "assets/holdon.mp3",
-      description:"Un estilo de música fresco y que le gustará al mundo, el Future Bass"
+      description:"Un estilo de música fresco y que le gustará al mundo, el Future Bass",
+      duration:"03:56",
     },
     {
       id: 5,
@@ -61,7 +65,8 @@ export class CancionesService {
       fecha: "2003",
       album: "Lo Más Lejos",
       url: "assets/soldaditomarinero.mp3",
-      description:"Un resumen a la cultura española, simplemente con esta canción."
+      description:"Un resumen a la cultura española, simplemente con esta canción.",
+      duration:"03:59",
     },
   ];
 
@@ -87,5 +92,6 @@ export interface Cancion{
   album: string;
   url: string;
   description: string;
+  duration: string;
 
 };
